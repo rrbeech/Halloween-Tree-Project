4412018bbd6d749697e0381f69ebc670dc098d37
rrbeech <autumnartist@gmail.com>
2021-08-10 10:12:12 -0400

    Testing the version control

---
+++ Halloween Tree/Assets/Plugins/signin.bat
@@ -0,0 +1,3 @@
+echo off
+title Sign In to AltspaceVR
+curl -v -d "user[email]=autumnartist@gmail.com&user[password]=vwOFP857" https://account.altvr.com/users/sign_in.json -c cookie
---
+++ Halloween Tree/Assets/Plugins/upload.bat
@@ -0,0 +1,2 @@
+echo off
+curl -v -b cookie -X PUT -F "space_template[zip]=@C:\Users\autum\OneDrive\Desktop\Creating VR Worlds\Halloween Tree\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1797518853056495775.json
